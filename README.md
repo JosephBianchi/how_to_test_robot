e4fd913b2d7db5a227b0e2221a31239bfd89c058
Joe Bianchi <joseph.bianchi3@gmail.com>
2017-09-16 12:37:03 -0400

    done testing

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 [See assignment in Alexa](https://alexa.bitmaker.co/wdi/may-2017/assignments/2591/latest)
+
+Try running ruby test_robot.rb. If all is well, you should see 8 skipped tests and no error messages.
+Read the code in robot.rb and the descriptions of the unit test to be implemented in test_robot.rb.
+One by one, remove skip from each unit test and in its place write your test code. In the end your test output should read
+8 runs, 8 assertions, 0 failures, 0 errors, 0 skips
--- test_robot.rb
+++ test_robot.rb
@@ -5,75 +5,94 @@ require './robot.rb'
 class TestRobot < MiniTest::Test
 
   def test_that_foreign_robot_needing_repairs_sent_to_station_1
-    skip
+    # skip
     # arrange
-
+    robot = Robot.new
+    robot.foreign_model = true
+    robot.needs_repairs = true
     # act
-
+    output = robot.station
     # assert
+    assert_equal(1, output)
   end
 
   def test_that_vintage_robot_needing_repairs_sent_to_station_2
-    skip
+    # skip
     # arrange
-
+    robot = Robot.new
+    robot.vintage_model = true
+    robot.needs_repairs = true
     # act
-
+    output = robot.station
     # assert
+    assert_equal(2, output)
   end
 
   def test_that_standard_robot_needing_repairs_sent_to_station_3
-    skip
+    # skip
     # arrange
-
+    robot = Robot.new
+    robot.needs_repairs = true
     # act
-
+    output = robot.station
     # assert
+    assert_equal(3, output)
   end
 
   def test_that_robot_in_good_condition_sent_to_station_4
-    skip
+    # skip
     # arrange
-
+    robot = Robot.new
+    robot.needs_repairs = false
     # act
-
+    output = robot.station
     # assert
+    assert_equal(4, output)
   end
 
   def test_prioritize_tasks_with_empty_todo_list_returns_negative_one
-    skip
+    # skip
     # arrange
-
+    robot = Robot.new
+    robot.needs_repairs = false
     # act
-
+    output = robot.prioritize_tasks
     # assert
+    assert_equal(-1, output)
   end
 
   def test_prioritize_tasks_with_todos_returns_max_todo_value
-    skip
+    # skip
     # arrange
-
+    robot = Robot.new
+    robot.needs_repairs = true
+    robot.todos = [1, 3, 2]
     # act
-
+    output = robot.prioritize_tasks
     # assert
+    assert_equal(3, output)
   end
 
   def test_workday_on_day_off_returns_false
-    skip
+    # skip
     # arrange
-
+    robot = Robot.new
+    robot.day_off = "sat"
     # act
-
+    output = robot.workday?("sat")
     # assert
+    assert_equal(false, output)
   end
 
   def test_workday_not_day_off_returns_true
-    skip
+    # skip
     # arrange
-
+    robot = Robot.new
+    robot.day_off = "sat"
     # act
-
+    output = robot.workday?("mon")
     # assert
+    assert_equal(true, output)
   end
 
 end
